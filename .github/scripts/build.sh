339b78ed3986bf6c09946a52801be8f4b79759d0
DeBendeBurcht <102698576+DeBendeBurcht@users.noreply.github.com>
2024-07-18 16:19:47 +0200

    more shenanigans

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,17 +8,20 @@ docker run --rm --privileged \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
     --target /data \
-    --docker-user "${DOCKER_USER}" \
-    --docker-password "${DOCKER_PASSWORD}" \
+    --docker-user "${DOCKERHUB_USER}" \
+    --docker-password "${DOCKERHUB_PASSWORD}" \
     --no-latest \
     --${ARCH:-all}
 
 # Extract the built image name
-IMAGE_NAME=$(docker images --filter=reference='bradsjm/hyperion-ng-addon-*' --format "{{.Repository}}:{{.Tag}}" | head -n 1)
-NEW_IMAGE_NAME="${DOCKER_USER}/hyperion-ng-addon-${ARCH}:${GITHUB_SHA}"
+IMAGE_NAME=$(docker images --filter=reference="${DOCKERHUB_USER}/hyperion-ng-addon-*" --format "{{.Repository}}:{{.Tag}}" | head -n 1)
+NEW_IMAGE_NAME="${DOCKERHUB_USER}/hyperion-ng-addon-${ARCH}:${GITHUB_SHA}"
 
 # Tag the image with the correct repository name
 docker tag ${IMAGE_NAME} ${NEW_IMAGE_NAME}
+
+# Docker login using the correct Docker Hub credentials
+echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USER}" --password-stdin
 
 # Push the image to Docker Hub
 docker push ${NEW_IMAGE_NAME}
