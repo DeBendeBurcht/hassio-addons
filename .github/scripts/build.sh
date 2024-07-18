420e6c71819dddcd04fb4d391ec09c0388c1aaaf
DeBendeBurcht <102698576+DeBendeBurcht@users.noreply.github.com>
2024-07-18 16:31:36 +0200

    back to basics, cuz it still dont wanna work

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,7 +2,6 @@
 set -e
 ARCH=$1
 
-# Build the image
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
@@ -12,16 +11,3 @@ docker run --rm --privileged \
     --docker-password "${DOCKERHUB_PASSWORD}" \
     --no-latest \
     --${ARCH:-all}
-
-# Extract the built image name
-IMAGE_NAME=$(docker images --filter=reference="${DOCKERHUB_USER}/hyperion-ng-addon-*" --format "{{.Repository}}:{{.Tag}}" | head -n 1)
-NEW_IMAGE_NAME="${DOCKERHUB_USER}/hyperion-ng-addon-${ARCH}:${GITHUB_SHA}"
-
-# Tag the image with the correct repository name
-docker tag ${IMAGE_NAME} ${NEW_IMAGE_NAME}
-
-# Docker login using the correct Docker Hub credentials
-echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USER}" --password-stdin
-
-# Push the image to Docker Hub
-docker push ${NEW_IMAGE_NAME}
