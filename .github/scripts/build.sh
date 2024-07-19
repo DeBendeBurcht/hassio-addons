26d82b2f0913ca25cb0caf724b285c9e19d5cebd
DeBendeBurcht <102698576+DeBendeBurcht@users.noreply.github.com>
2024-07-19 02:26:58 +0200

    adjustment

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,10 +2,11 @@
 set -e
 ARCH=$1
 
+# Ensure correct path and mounting
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
-    --target /data/build-output \
+    --target /data \
     --test \
     --${ARCH:-all}
