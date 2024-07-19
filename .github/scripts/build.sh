71435d92b3a88e53fc005af7581177be41e8f4a1
DeBendeBurcht <102698576+DeBendeBurcht@users.noreply.github.com>
2024-07-19 02:24:08 +0200

    test tag local build save

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,10 +6,6 @@ docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
-    --target /data \
-    --docker-user "${DOCKERHUB_USER}" \
-    --docker-password "${DOCKERHUB_PASSWORD}" \
-    --image "debendeburcht/hyperion-ng-addon-amd64:2.0.16" \
-    --release-tag \
-    --no-latest \
+    --target /data/build-output \
+    --test \
     --${ARCH:-all}
